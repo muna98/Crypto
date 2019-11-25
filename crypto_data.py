7375df2266762fe1790bcd0db20fe080e72186e7
Lavinia <hbie1@sheffield.ac.uk>
2019-11-25 09:13:49 +0000

    step1

--- crypto_data.py
+++ crypto_data.py
@@ -1,3 +1,14 @@
+currency = input('Do you know what a crytocurrency is? (y/n) ?')
+if currency =='y':
+   cryptocurrency_name = input('What kinds of cryptocurrenies do you know?')
+   print('You know {}'.format(cryptocurrency_name))
+else:
+    print('A cryptocurrency is a digital asset designed to work as a medium of exchange that uses strong cryptography to secure financial transactions, control the creation of additional units, and verify the transfer of assets.')
+
+
+
+
+
 import requests
 from pprint import pprint
 
@@ -8,3 +19,4 @@ print(response)
 
 currencies = response.json()
 pprint(currencies)
+
