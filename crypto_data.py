7d8fa6ca1aa107f9149c74a8fd1238aa9bbbfd0a
Sumayia Ashraf <ashraf.sumayia@gmail.com>
2019-11-25 09:12:10 +0000

    reading specific parts

--- crypto_data.py
+++ crypto_data.py
@@ -1,6 +1,8 @@
 import requests
 from pprint import pprint
 
+crypto_id = input('Which currency do you want to look up?')
+
 url = 'https://api.coinmarketcap.com/v1/ticker/'
 
 response = requests.get(url)
@@ -8,3 +10,9 @@ print(response)
 
 currencies = response.json()
 pprint(currencies)
+
+print(currency['id'])
+print(currency['name'])
+print(currency['rank'])
+print(currency['price_usd'])
+
