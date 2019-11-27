02ecd0a13cafc40f7ca37a02aa8e84e6ef52d168
Sumayia Ashraf <ashraf.sumayia@gmail.com>
2019-11-27 18:24:15 +0000

    reordered codes

--- crypto_data.py
+++ crypto_data.py
@@ -1,13 +1,43 @@
 <<<<<<< HEAD
-import requestsimport pprintimport numpy as npcurrency = input('Do you know what a crytocurrency is? (y/n) ?')if currency =='y':   cryptocurrency_name = input('Which cryptocurrency do you know?')   print('You know {}'.format(cryptocurrency_name))else:    print('A cryptocurrency is a digital asset designed to work as a medium of exchange that uses strong cryptography to secure financial transactions, control the creation of additional units, and verify the transfer of assets.')crypto_id = input('Which currency do you want to look up?')url = 'https://api.coinmarketcap.com/v1/ticker/'response = requests.get(url)print(response)currencies = response.json()cryptonames = []for currency in currencies:    cryptonames.append(currency['id'].lower())print(cryptonames)name_index = cryptonames.index(crypto_id.lower())print(name_index)coin = currencies[name_index]print(coin)print(coin['name'])print(coin['rank'])print(coin['price_usd'])print(coin['percent_change_24h'])print(coin['percent_change_1h'])from prettytable import PrettyTabletable = PrettyTable()for currency in currencies:    name = (coin)[currency]['name']    price = (coin)[currency]['quotes']['USD']['price']    change_1h = currencies[currency]['quotes']['USD']['percent_change_1h']    change_24h = currencies[currency]['quotes']['USD']['percent_change_24h']    change_7d = currencies[currency]['quotes']['USD']['percent_change_7d']    table.add_row([name,price,change_1h,change_24h,change_7d])table.field_names = ["Name","Price","Change 1h","Change 24h","Change 7d"]table.sortby = "Change 24h"table.reversesort = Trueprint(table)
-=======
 import requests
-from pprint import pprint
+import pprint
+import numpy as np
+
+currency = input('Do you know what a crytocurrency is? (y/n) ?')
+if currency =='y':
+   cryptocurrency_name = input('Which cryptocurrency do you know?')
+   print('You know {}'.format(cryptocurrency_name))
+else:
+    print('A cryptocurrency is a digital asset designed to work as a medium of exchange that uses strong cryptography to secure financial transactions, control the creation of additional units, and verify the transfer of assets.')
+
+
+crypto_id = input('Which currency do you want to look up?')
 
 url = 'https://api.coinmarketcap.com/v1/ticker/'
+
 response = requests.get(url)
+print(response)
+
 currencies = response.json()
-pprint(currencies)
+
+cryptonames = []
+for currency in currencies:
+    cryptonames.append(currency['id'].lower())
+print(cryptonames)
+
+name_index = cryptonames.index(crypto_id.lower())
+print(name_index)
+
+coin = currencies[name_index]
+print(coin)
+print(coin['name'])
+print(coin['rank'])
+print(coin['price_usd'])
+print(coin['percent_change_24h'])
+print(coin['percent_change_1h'])
+
+
+
 
 # Top 10 crypotocurrencies for price(in USD).
 print('-' * 50)
@@ -59,5 +89,30 @@ def usd_converter(n, p):
 usd_converter(n, p)
 
 
+from prettytable import PrettyTable
 
+table = PrettyTable()
+
+for currency in currencies:
+    name = (coin)[currency]['name']
+    price = (coin)[currency]['quotes']['USD']['price']
+    change_1h = currencies[currency]['quotes']['USD']['percent_change_1h']
+    change_24h = currencies[currency]['quotes']['USD']['percent_change_24h']
+    change_7d = currencies[currency]['quotes']['USD']['percent_change_7d']
+
+    table.add_row([name,price,change_1h,change_24h,change_7d])
+
+table.field_names = ["Name","Price","Change 1h","Change 24h","Change 7d"]
+table.sortby = "Change 24h"
+table.reversesort = True
+print(table)
+
+=======
+import requests
+from pprint import pprint
+
+url = 'https://api.coinmarketcap.com/v1/ticker/'
+response = requests.get(url)
+currencies = response.json()
+pprint(currencies)
 >>>>>>> master
